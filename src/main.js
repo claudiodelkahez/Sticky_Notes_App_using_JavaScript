1b3787eadd4292fcc67a27a1c13d3c0dc96aa7f9
Claudio Delkahez <cdelkahez.dev@gmail.com>
2022-01-03 13:56:46 -0300

    Cree la estructura basica de todas las funciones necesarias

---
+++ src/main.js
@@ -0,0 +1,23 @@
+function getNotes() {
+
+}
+
+function saveNotes(notes) {
+
+}
+
+function createNotesElements(id, content) {
+
+}
+
+function addNote() {
+
+}
+
+function updateNotes(id, newContent) {
+
+}
+
+function deleteNotes(id, element) {
+
+}
