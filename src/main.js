930366015cebb5f00d14f784851a4374ee7b4f47
Claudio Delkahez <cdelkahez.dev@gmail.com>
2022-01-03 14:38:53 -0300

    createNoteElementes funcion lista

--- src/main.js
+++ src/main.js
@@ -4,26 +4,48 @@ const addNoteButton = notesContainer.querySelector(".add-note")
 
 
 
-function getNotes() {
+function getNote() {
     return JSON.parse(localStorage.getItem("stickynotes-notes") || "[]");
 }
 
 function saveNotes(notes) {
-localStorage.setItem("stickynotes-notes", JSON.stringify(notes));
+    localStorage.setItem("stickynotes-notes", JSON.stringify(notes));
 }
 
-function createNotesElements(id, content) {
+function createNoteElements(id, content) {
+    const element = document.createElement("textarea");
+    //Javascript representation of a JavaScript textarea 
+    element.classList.add("note");
+    //add the CSS rulls are goint to be aplied to de the new element
+    element.vale = content;
+    element.placeholder = "Empty sticky note";
+    // default text inside the thext area
 
+    element.addEventListener("change", () => {
+        updateNote(id, element.value);
+    });
+    //react to when the user double clicks with the intention to delete a note 
+    element.addEventListener("dblclick", () =>{
+        const doDelete = confirm("Are you sure you wish to delete this note?");
+        if(doDelete) {
+            deleteNote(id, element);
+        }
+    });
+
+
+    return element;
 }
 
 function addNote() {
 
 }
 
-function updateNotes(id, newContent) {
-
+function updateNote(id, newContent) {
+    console.log("updating note...");
+    console.log(id, newContent);
 }
 
-function deleteNotes(id, element) {
-
+function deleteNote(id, element) {
+    console.log("Deleting note note...");
+    console.log(id, newContent);
 }
