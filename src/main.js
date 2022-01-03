0120cd5127b6a7e977b6724098cd72aa5c33998f
Claudio Delkahez <cdelkahez.dev@gmail.com>
2022-01-03 14:05:48 -0300

    getNotes funcion lista

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-function getNotes() {
+const notesContainer = document.getElementById("app");
+const addNoteButton = notesContainer.querySelector(".add-note")
 
+
+
+
+function getNotes() {
+    return JSON.parse(localStorage.getItem("stickynotes-notes") || "[]")
 }
 
 function saveNotes(notes) {
